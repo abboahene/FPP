7f45ed26d9f1b84fea44d1190c301af0779556a8
Alfred <alfredboahene.7@gmail.com>
2024-03-19 15:34:22 -0500

    update local nested inner class

--- src/NestedClasses/NonStaticNestedClasses/LocalNestedClasses/MyClass.java
+++ src/NestedClasses/NonStaticNestedClasses/LocalNestedClasses/MyClass.java
@@ -3,4 +3,12 @@ package NestedClasses.NonStaticNestedClasses.LocalNestedClasses;
 public class MyClass {
 
     // local nested classes are classes found within a method body
+
+    public static void main(String[] args) {
+
+        //MyInnerClass is a local nested inner class
+        class MyInnerClass{
+
+        }
+    }
 }
